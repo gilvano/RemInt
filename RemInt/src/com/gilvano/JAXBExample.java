fd4f3289382f57483e97b03b61f758f06e2f5e00
gilvanopiontkoski <gilvanopiontkoski@ciss.com.br>
2014-01-22 15:21:43 -0200

    JAXB Marshaller

---
+++ RemInt/src/com/gilvano/JAXBExample.java
@@ -0,0 +1,33 @@
+package com.gilvano;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+public class JAXBExample {
+
+	public static void main(String[] args) {
+
+		Customer customer = new Customer();
+		customer.setId(100);
+		customer.setName("Gilvano");
+		customer.setAge(32);
+
+		try {
+			File file = new File("C:\\file.xml");
+
+			JAXBContext jaxbcontext = JAXBContext.newInstance(Customer.class);
+			Marshaller jaxbMarshaller = jaxbcontext.createMarshaller();
+
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			jaxbMarshaller.marshal(customer, file);
+			jaxbMarshaller.marshal(customer, System.out);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
